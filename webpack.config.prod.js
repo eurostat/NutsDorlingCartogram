d7b22d4fc75baabbca8ad36f833c5afe461b4a2d
Joe Davies <jwd101@gmail.com>
2020-09-08 09:56:13 +0200

    new prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,8 +14,12 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
-    ]
+    ],
   },
   watch: false,
   optimization: {
