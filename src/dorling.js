ed16a21756f5564fa857eb30e59f81e3d3849d3e
Joe Davies <jwd101@gmail.com>
2020-09-28 13:32:16 +0200

    legend collapsable mode height threshold changed from 600px to 700px

--- src/dorling.js
+++ src/dorling.js
@@ -36,7 +36,7 @@ export function dorling() {
   out.graticule_ = false;
   out.nutsBorderColor_ = "grey";
   out.showLegendWidthThreshold_ = 850;
-  out.showLegendHeightThreshold_ = 600;
+  out.showLegendHeightThreshold_ = 700;
 
   //d3 force
   // out.circleExaggerationFactor_ = 1.2; //deprecated
